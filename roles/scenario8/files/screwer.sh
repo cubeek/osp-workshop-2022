d4811622468e9726ffcaca02214378713dbb3600
Jakub Libosvar <jakub.libosvar@gmail.com>
2023-09-28 12:01:13 -0400

    scenario8: cleanup screwer processes

--- roles/scenario8/files/screwer.sh
+++ roles/scenario8/files/screwer.sh
@@ -3,6 +3,11 @@
 while true; do
     if ip net | grep -q ovnmeta; then
         iface_id=$(ip net e $(ip net | awk '/ovnmeta-/{ print $1 }' | head -n1) ip a | awk '/: tap/{ print $2 }' | tr -d ":" | cut -d @ -f 2 | tr -d "if")
+        while [ "x$iface_id" == "x" ]; do
+            echo "$(date): ovnmeta- namespace exists but didn't find the ovnmeta port yet, waiting" >> /tmp/screwer.log
+            sleep 1
+            iface_id=$(ip net e $(ip net | awk '/ovnmeta-/{ print $1 }' | head -n1) ip a | awk '/: tap/{ print $2 }' | tr -d ":" | cut -d @ -f 2 | tr -d "if")
+        done
         iface=$(ip a | awk "/$iface_id: tap/{ print \$2 }" | cut -d @ -f1)
         ofport=$(ovs-vsctl get interface $iface ofport)
         flow=$(ovs-ofctl dump-flows br-int table=65 | grep "output:${ofport}$" | sed 's/.*\(cookie=0x[0-9a-f]*\), .*priority=[0-9]*,\([^ ]*\) actions.*/\1 \2/')
